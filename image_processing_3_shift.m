36aa8b89dfa820ae4b403131930f89f3975f761f
Gülşah Seher <70262153+gulsahseher@users.noreply.github.com>
2023-06-06 16:45:27 +0300

    Update image_processing_3_shift.m
    Commit line added

--- image_processing_3_shift.m
+++ image_processing_3_shift.m
@@ -3,15 +3,23 @@ clear all;
 clear;
 close all;
 
+% This code is an example where an image is processed by shifting or translating it by a certain amount.
+
 I1=imread("C:\Users\GULSAH\Documents\MATLAB\Image Processing\low-contrast-lena.jpg")
 figure,imshow(uint8(I1)),title("org")
 [h,w]=size(I1);
 
 x=10;
 y=10;
+
+% Create a shifted image I2 by adding padding
+
 I2=zeros(h+10,w+10);
 I2(y+1:y+h,x+1:x+w)=I1;
+
 figure,imshow(uint8(I2)),title("shifted")
+
+% Shift the image I1 using a loop
 
 x=10;
 y=10;
