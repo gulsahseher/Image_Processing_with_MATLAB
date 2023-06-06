2f079115340c9916d63b90f477f54f178681e834
Gülşah Seher <70262153+gulsahseher@users.noreply.github.com>
2023-06-06 15:04:47 +0300

    Update image_processing_4_zoom.m

--- image_processing_4_zoom.m
+++ image_processing_4_zoom.m
@@ -12,21 +12,20 @@ I3=zeros(2*h,2*w);
 
 for i=1:h-1
     for j=1:w-1
-
         I2(i*2,j*2)=I(i,j);
         I2(i*2-1,j*2)=I(i,j);
         I2(i*2,j*2-1)=I(i,j);
         I2(i*2-1,j*2-1)=I(i,j);  
 
-        I3(i*2,j*2)=(I(i,j)+I(i+1,j)+I(i,j+1)+I(i+1,j+1))/4; %%sağ alt
-        I3(i*2-1,j*2)=(I(i,j)+I(i+1,j))/2; %%sağ üst
-        I3(i*2-1,j*2-1)=I(i,j); %%sol üst
+        I3(i*2,j*2)=(I(i,j)+I(i+1,j)+I(i,j+1)+I(i+1,j+1))/4; %%bottom right
+        I3(i*2-1,j*2)=(I(i,j)+I(i+1,j))/2; %%top right
+        I3(i*2-1,j*2-1)=I(i,j); %%top left
         I3(i*2,j*2-1)=(I(i,j)+I(i,j+1))/2;
     end
 end
 
-figure,imshow(uint8(I2)),title("ZOOM-1.YÖNTEM")
-figure,imshow(uint8(I3)),title("ZOOM-2.YÖNTEM")
+figure,imshow(uint8(I2)),title("ZOOM-First Method")
+figure,imshow(uint8(I3)),title("Second Method")
 
 r=3;
 I4=imresize(I,r,"nearest");
