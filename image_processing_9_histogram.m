778c748b0a70b3357c77a926f6b0acfae271ae2c
Gülşah Seher <70262153+gulsahseher@users.noreply.github.com>
2023-06-06 20:33:53 +0300

    Update image_processing_9_histogram.m

--- image_processing_9_histogram.m
+++ image_processing_9_histogram.m
@@ -3,28 +3,34 @@ clear all;
 clear;
 close all;
 
+% Histogram represents the frequency of occurrence of each grayscale level ([0, 255]) in the image.
+% It provides information about the distribution of pixels in the image.
+% It is commonly used in image enhancement.
+% Histogram normalization reveals the probabilities of grayscale levels occurring in the image.
+
 I=imread("low-contrast-lena.jpg");
 figure,imshow(uint8(I))
 [w,h]=size(I);
+
+% Calculates the histogram of an image by counting the occurrence of each pixel value.
 
 % histog=zeros(1,256);
 % 
 % for i=1:w
 %     for j=1:h
-%         histog(I(i,j)+1)=histog(I(i,j)+1)+1;
+%         histog(I(i,j)+1)=histog(I(i,j)+1)+1; %% Iterate over each pixel in the image and increment the count of the corresponding bin in the histogram vector.
 %     end
 % end
 
-[hm1]=histogram_bulma(I);
+[hm1]=find_histogram(I);
 % [hm1]=imhist(I)
 figure, plot(hm1)
 figure, bar(hm1)
 
-I2=imadjust(I)
+I2=imadjust(I) 
 figure,imshow(I2),title("enhancement_image")
 
-[hm2]=histogram_bulma(I2);
+[hm2]=find_histogram(I2);
 % [hm2]=imhist(I2)
 figure, plot(hm2)
 figure, bar(hm2)
-
