8fcba5145920bcb8e3e464a08996e65c7a1b229c
Gülşah Seher <70262153+gulsahseher@users.noreply.github.com>
2023-06-06 19:55:47 +0300

    Update image_processing_7_contrast_stretching.m
    Comment line added

--- image_processing_7_contrast_stretching.m
+++ image_processing_7_contrast_stretching.m
@@ -1,13 +1,18 @@
-%%contrast stretching
-
 clc;
 clear all;
 clear;
 close all;
 
+% Contrast Stretching
+
+% Contrast stretching, also known as contrast enhancement or intensity rescaling, is a technique used to improve the contrast in an image. 
+% It involves expanding the range of pixel values in an image to cover a wider dynamic range, which results in a more visually appealing and better-contrasted image.
+% The basic idea behind contrast stretching is to redistribute the pixel values in the image so that they span the entire available range. 
+% This is achieved by mapping the minimum and maximum pixel values in the original image to the minimum and maximum values of the desired output range.
+
 I=double(rgb2gray(imread("x-ray.jpg")));
 
-I2=255*(I-min(min(I)))/(max(max(I))-min(min(I)));
+I2=255*(I-min(min(I)))/(max(max(I))-min(min(I))); % Perform contrast stretching
 
 figure,imshow(uint8(I))
 figure,imshow(uint8(I2))
