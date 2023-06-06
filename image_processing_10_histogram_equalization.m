29dc83cf9d77854ed5866c27455bfd112f56c209
Gülşah Seher <70262153+gulsahseher@users.noreply.github.com>
2023-06-06 20:53:25 +0300

    Update image_processing_10_histogram_equalization.m

--- image_processing_10_histogram_equalization.m
+++ image_processing_10_histogram_equalization.m
@@ -3,14 +3,23 @@ clear all;
 clear;
 close all;
 
+% These codes demonstrate the enhancement of a low-contrast image by applying histogram equalization. 
+% the histogram of the image --> the normalized histogram --> the cumulative distribution function --> a lookup table --> mapping pixel values --> the histogram-equalized image 
+
 I=imread("low-contrast-lena.jpg");
 [w,h]=size(I);
 
+% Compute and display the histogram
+
 hm1=imhist(I);
 figure,bar(hm1),title("imhist")
-norm_hist=hm1/(w*h);
 
+% Compute and display the normalized histogram (probability density function)
+
+norm_hist=hm1/(w*h);
 figure,bar(norm_hist),title("norm_hist-pdf")
+
+% Compute and display the cumulative distribution function
 
 c=zeros(1,256);
 c(1)=hm1(1);
@@ -20,13 +29,17 @@ end
 
 figure,bar(c),title("-cdf")
 
-LUT=zeros(1,256);
 
+% Create the lookup table for histogram equalization
+
+LUT=zeros(1,256);
 cdf_min=min(c);
+
 for v=1:256
-    LUT(v)=round(255*(c(v)-cdf_ ...
-        min)/(w*h-cdf_min));
+    LUT(v)=round(255*(c(v)-cdf_min)/(w*h-cdf_min));
 end
+
+% Apply histogram equalization to the image
 
 I2=zeros(w,h);
 for i=1:w
